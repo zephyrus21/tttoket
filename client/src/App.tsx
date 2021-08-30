3bfc50ec6b31618f92b814a39e46e4c90db6de9a
zephyrus21 <pandey.piyus@gmail.com>
2021-08-30 12:45:56 +0530

    adding connection service

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,37 @@
 import React, { useEffect } from "react";
 import { io } from "socket.io-client";
+import styled from "styled-components";
+
+import "./App.css";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em;
+`;
+
+const WelcomText = styled.h1`
+  color: #8e44ad;
+`;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const App: React.FC = ({}) => {
-  const connect = () => {
-    const socket = io("http://localhost:9000");
-  };
-
-  useEffect(() => {
-    connect();
-  }, []);
-
-  return <div>Hey</div>;
+  return (
+    <AppContainer>
+      <WelcomText>Tic-Tac-Toe</WelcomText>
+      <MainContainer>Hey</MainContainer>
+    </AppContainer>
+  );
 };
 
 export default App;
---
+++ server/src/api/controllers/roomController.ts
@@ -0,0 +1,39 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnMessage,
+  SocketController,
+  SocketIO,
+} from "socket-controllers";
+import { Server, Socket } from "socket.io";
+
+@SocketController()
+export class RoomController {
+  @OnMessage("join_game")
+  public async joinGame(
+    @SocketIO() io: Server,
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() message: any
+  ) {
+    console.log(message);
+    const connectedSockets = io.sockets.adapter.rooms.get(message.roomId);
+    const socketRooms = Array.from(socket.rooms.values()).filter(
+      (r) => r !== socket.id
+    );
+
+    //* If the room is full
+    if (
+      socketRooms.length > 0 ||
+      (connectedSockets && connectedSockets.size === 2)
+    ) {
+      socket.emit("room_join_error", {
+        error: "Room is full!",
+      });
+    }
+    //* If the user can join the room
+    else {
+      await socket.join(message.roomId);
+      socket.emit("room_joined");
+    }
+  }
+}
